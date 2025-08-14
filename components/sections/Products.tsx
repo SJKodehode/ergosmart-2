f1a68a3ffdc7487f715fa3af540232e25b7b009b
Jorgen <sjkodehode2025@gmail.com>
2025-08-14 17:06:08 +0200

    new version, optimized more

--- components/sections/Products.tsx
+++ components/sections/Products.tsx
@@ -23,7 +23,7 @@ export function Products() {
           whileInView={{ opacity: 1, y: 0 }}
           viewport={{ once: true }}
           transition={{ duration: 0.8 }}
-          className="text-center mb-16"
+          className="text-center xl:mb-16"
         >
           <h2 className="text-4xl md:text-5xl font-bold mb-6">Våre produkter</h2>
           <p className="text-base md:text-lg max-w-3xl mx-auto text-muted-background">
@@ -31,13 +31,13 @@ export function Products() {
             plasserte gripehåndtak og forsterkede sømmer.
           </p>
         </motion.div>
-      <div className="grid grid-cols-1 xl:grid-cols-2 gap-8">
+      <div className="grid grid-cols-1">
         <motion.div
           initial={{ opacity: 0, scale: 0.9 }}
           whileInView={{ opacity: 1, scale: 1 }}
           viewport={{ once: true }}
           transition={{ duration: 0.8 }}
-          className="max-w-3xl mx-auto"
+          className="max-w-3xl mx-auto hidden"
           >
           <Image
             src={'/assets/resized/product-collection.jpg'}
@@ -52,8 +52,8 @@ export function Products() {
         </motion.div>
 
 
-        <div className="grid grid-cols-3 gap-2 md:gap-8 xl:mt-3 mt-16">
-          {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((item, index) => (
+        <div className="grid grid-cols-2 lg:grid-cols-5 gap-4 max-w-7xl mx-auto lg:gap-8 xl:mt-3 mt-16">
+          {[ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item, index) => (
             <motion.div
             key={index}
             initial={{ opacity: 0, y: 30 }}
@@ -62,11 +62,11 @@ export function Products() {
             transition={{ duration: 0.6, delay: index * 0.1 }}
             >
               <Image
-                src={`/assets/resized/washcloth-${1 + index}_converted.avif`}
+                src={`/assets/resized/washcloth-${1+ index}_converted.avif`}
                 alt={`ErgoSmart produkt ${item}`}
                 className="rounded-xl shadow-lg hover:shadow-xl transition-shadow duration-300 opacity-100"
                 loading="lazy"
-                width={181}
+                
                 classNames={{
                   wrapper: "w-full aspect-rectangle"
                 }}
